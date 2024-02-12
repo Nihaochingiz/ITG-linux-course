639e8da44b2cb5576867f0a0ae214fb02aaf78ee
Ilya Fofanov <66513936+Nihaochingiz@users.noreply.github.com>
2024-02-12 17:58:43 +0300

    Delete second_task.bash

--- second_task.bash
+++
@@ -1,8 +0,0 @@
-#уже имеем именованный канал my_pipe
-#архивируем
-tar czf - -T my_pipe > archive.tar.gz &
-# в новом терминале 
-cat my_pipe > my_pipe
-#Вывод
-#[3] 100
-#[2]   Done                    cat < my_pipe > output.txt
